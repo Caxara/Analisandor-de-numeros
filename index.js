20b6a1e6bf4ed1367bee989613bd3e6bd297e31b
Jonathan Mendes <carisjhow@gmail.com>
2022-10-26 09:52:42 -0300

    AtualizaçãoJavaScript

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
+let num = document.querySelector('input#num');
+let lista = document.querySelector('select#flista');
+let res = document.querySelector('div#res');
+let valores = [];
+
+function isNumero(n){
+    if(Number(n)  >= 1 && Number(n) <= 100){
+        return true
+    }else {
+        return false
+    }
+}
+
+function inLista(n, l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
+    }
+}
+
 function adicionar(){
-    var numero = document.getElementById('num');
-    
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        item.style.fontFamily = 'VT323';
+        lista.appendChild(item)
+    }
+    else{
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar(){
+    if(valores.length == 0){
+        window.alert('Adicione valores antes de finalizar!')
+    }else{
+        let tot = valores.length
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${tot} números cadastrados.</p>`
+    }
 }
 
