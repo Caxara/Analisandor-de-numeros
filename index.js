6b47c23a3d16c01831fb31928b61571a75c11a82
Jonathan Mendes <carisjhow@gmail.com>
2022-10-26 10:28:56 -0300

    Finished(I think it)

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ function adicionar(){
         item.text = `Valor ${num.value} adicionado.`
         item.style.fontFamily = 'VT323';
         lista.appendChild(item)
+        res.innerHTML = ''
     }
     else{
         window.alert('Valor inválido ou já encontrado na lista.')
@@ -38,9 +39,26 @@ function finalizar(){
     if(valores.length == 0){
         window.alert('Adicione valores antes de finalizar!')
     }else{
-        let tot = valores.length
+        let totalNum = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media= 0
+
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior)
+            maior = valores[pos]
+            if(valores[pos] < menor)
+            menor = valores[pos]
+        }
+        media = soma / totalNum
         res.innerHTML = ''
-        res.innerHTML += `<p>Ao todo temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>Ao todo temos ${totalNum} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
     }
 }
 
